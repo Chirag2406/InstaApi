438fa7292dec3fbecf097ceb3dee82ae396824d8
Chirag2406 <chiragsharma3091@gmail.com>
2022-11-18 11:10:15 +0530

    Added index.html to display images

---
+++ .history/src/components/login_20221114114613.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./login.css";
+const Login = () => {
+    return (
+        <div className="cover">
+            <h1>Sign in</h1>
+            <form id="myForm">
+                <input type="text" id="username" placeholder="Username" />
+                <div type="submit" className="login-btn">Login</div>
+            </form>
+            <p className="text">Or Login Using</p>
+            <div className="alt-login">
+                <div className="google"></div>
+                <div className="instagram"></div>
+            </div>
+        </div>
+    )
+}
+export default Login;
